bec1f0d1152a275a1e4ef3bd6d6370e5f71be8b4
Jack Faller <jack.t.faller@gmail.com>
2025-11-10 05:14:02 +0000

    Adds some more keywords and tweaks use order

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -3,22 +3,18 @@
 (lambda "." @punctuation.delimiter)
 (number) @number
 [(comment) (preamble)] @comment
-((symbol) @keyword
- (#match? @keyword "^_$"))
-((symbol) @constant.builtin
- (#match? @constant.builtin "^(true|false)$"))
-((symbol) @type
- (#match? @type "^[A-Z]"))
 (symbol) @variable
+((symbol) @constant.builtin (#match? @constant.builtin "^(true|false)$"))
 [(string) (long_string)] @string
 (string_escape) @string.special
 (call . (symbol) @function)
-((symbol) @operator
- (#match? @operator "^(\\+|-|\\*|/|=|!=|≠|>|<|>=|<=|^|'|\\||&)$"))
+((symbol) @operator (#match? @operator "^(\\+|-|\\*|/|=|!=|≠|>|<|>=|<=|^|'|\\||&)$"))
 (call . (symbol) @keyword
-      (#match? @keyword "^(module|package|define|let|language|match|if|and|or)$"))
+      (#match? @keyword "^(module|package|define|import|export|use|let|type|match|if|and|or)$"))
 (call . (symbol) @keyword . (symbol) @module
       (#match? @keyword "^(module|package)$"))
 (call . (symbol) @_at . _ . (symbol) @property (#match? @_at "^at$"))
 (pair . (comment) * . (symbol) @property)
 (pair ":" @property)
+((symbol) @keyword (#match? @keyword "^_$"))
+((symbol) @type (#match? @type "^[A-Z]"))
