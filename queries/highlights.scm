913762820faa62837d4a9d226f4a0a6c4ebcb542
Jack Faller <jack.t.faller@gmail.com>
2025-11-11 09:49:09 +0000

    Improves highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,5 +1,5 @@
 ["(" ")" "[" "]" "{" "}"] @punctuation.bracket
-(language) @attribute
+(language) @directive
 (lambda "." @punctuation.delimiter)
 (number) @number
 [(comment) (preamble)] @comment
@@ -7,14 +7,33 @@
 ((symbol) @constant.builtin (#match? @constant.builtin "^(true|false)$"))
 [(string) (long_string)] @string
 (string_escape) @string.special
-(call . (symbol) @function)
+(call . (comment)* . (symbol) @function)
 ((symbol) @operator (#match? @operator "^(\\+|-|\\*|/|=|!=|≠|>|<|>=|<=|^|'|\\||&)$"))
-(call . (symbol) @keyword
-      (#match? @keyword "^(module|package|define|import|export|use|let|type|macro|match|if|and|or)$"))
-(call . (symbol) @keyword . (symbol) @module
+(call . (comment)* . (symbol) @keyword
+      (#match? @keyword "^(module|package|define|import|export|use|document|let|type|macro|macro-variable|include|where|match|if|and|or|_)$"))
+(call . (comment)* . (symbol) @keyword . (comment)* . (symbol) @module
       (#match? @keyword "^(module|package)$"))
-(call . (symbol) @_at . _ . (symbol) @property (#match? @_at "^at$"))
-(pair . (comment) * . (symbol) @property)
-(pair ":" @property)
+(call . (comment)* . (symbol) @_at . (comment)* . _ . (comment)* . (symbol) @property (#match? @_at "^@$"))
+(pair ":" @directive . (comment)* . (symbol) @directive)
+(parameters [(symbol) @variable.definition
+             (pair ":" @variable.definition . (comment)* . (symbol) @variable.definition)])
+(call . (comment)* . (symbol) @keyword . (comment)* . (symbol) @variable.definition
+      (#equal? @keyword "let"))
+(call . (comment)* . (symbol) @keyword (symbol) @variable.definition
+      (#equal? @keyword "macro-variable"))
+(call . (comment)* . (symbol) @keyword
+      . (comment)* . _
+      (pair ":" @variable.definition . (comment) * . (symbol) @variable.definition)
+      (#eq? @keyword "where"))
+(call . (comment)* . (symbol) @keyword
+      . (comment)* . _
+      (pair ":" @variable.definition . (comment) * . (symbol) @variable.definition)
+      (#eq? @keyword "where"))
+(call . (comment)* . (symbol) @keyword
+      . (comment)* . (symbol) @function.definition
+      . (comment)* . [(lambda) (call . (comment)* . (symbol) @_macro (#eql? @_macro "macro"))]
+      (#equal? @keyword "let"))
+(call . (comment)* . (symbol) @keyword . (comment)* . (symbol) @module
+      (#match? @keyword "^(module|package)$"))
 ((symbol) @keyword (#match? @keyword "^_$"))
 ((symbol) @type (#match? @type "^[A-Z]"))
